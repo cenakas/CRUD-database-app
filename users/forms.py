a553b64aa8240dbbc9f94558eb1a8c0ce1da31d6
cenakas <cenakasia@gmail.com>
2019-01-27 00:08:41 +0100

    user authentication added

--- users/forms.py
+++ users/forms.py
@@ -5,10 +5,9 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields
+        fields = ('username', 'email')
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
-        
+        fields = ('username', 'email')
